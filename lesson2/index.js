0888782868c04c0083ca05c0054662350cc23cc1
Catharin <jekaterina.shashkina@gmail.com>
2023-06-27 20:19:48 +0300

    first task

---
+++ lesson2/index.js
@@ -0,0 +1,24 @@
+const div = document.querySelector('body');
+
+const label = document.createElement('label');
+
+const input = document.createElement('input');
+label.append(input);
+const textDiv = document.createElement('div');
+textDiv.style =
+  'width: 150px; height: 50px; border: 1px solid black; margin-top: 50px';
+const par = document.createElement('p');
+textDiv.append(par);
+div.append(label, textDiv);
+
+input.addEventListener('change', () => {
+  const text = input.value;
+  setTimeout(
+    function set(par, text) {
+      par.textContent = text;
+    },
+    1000,
+    par,
+    text,
+  );
+});
