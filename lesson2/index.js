f24685951f662183112efe00b84e2c773ef534fe
Catharin <jekaterina.shashkina@gmail.com>
2023-06-30 16:57:43 +0300

    with corrections

--- lesson2/index.js
+++ lesson2/index.js
@@ -1,7 +1,5 @@
 const div = document.querySelector('body');
-
 const label = document.createElement('label');
-
 const input = document.createElement('input');
 label.append(input);
 const textDiv = document.createElement('div');
@@ -11,14 +9,15 @@ const par = document.createElement('p');
 textDiv.append(par);
 div.append(label, textDiv);
 
-input.addEventListener('change', () => {
-  const text = input.value;
-  setTimeout(
-    function set(par, text) {
-      par.textContent = text;
-    },
-    300,
-    par,
-    text,
-  );
-});
+const debounce = (callback, delay) => {
+  let timeout;
+  return function () {
+    clearTimeout(timeout);
+    timeout = setTimeout(callback, delay);
+  };
+};
+
+const inputText = () => {
+  par.textContent = input.value;
+};
+input.addEventListener('change', debounce(inputText, 300));
