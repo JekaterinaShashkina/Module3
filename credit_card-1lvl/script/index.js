8d1a43476dc4a8a45c0d497da3c91264076ee3dd
Catharin <jekaterina.shashkina@gmail.com>
2023-08-03 22:37:22 +0300

    lvl1

--- credit_card-1lvl/script/index.js
+++ credit_card-1lvl/script/index.js
@@ -1,17 +1,33 @@
-import { el, setChildren } from '../node_modules/redom/dist/redom.es.js';
+import { el, setChildren } from 'redom';
+const creditcardutils = require('creditcardutils');
 
 const cardData = () => {
+  const cardNumber = el(
+    'span',
+    { className: 'card__number' },
+    'xxxx xxxx xxxx xxxx',
+  );
+  const cardName = el('span', { className: 'card__name' }, 'John Doe');
+  const cardDate = el('span', { className: 'card__date' }, '04/24');
+  const cardPersonal = el(
+    'div',
+    { className: 'card__personal' },
+    cardName,
+    cardDate,
+  );
+
+  const creditCard = el(
+    'div',
+    { className: 'credit-card' },
+    cardNumber,
+    cardPersonal,
+  );
   const card = el(
     'div',
     { className: 'card' },
     el('p', { className: 'secure' }, 'Secure Checkout'),
-    el('div', { className: 'credit-card' }, [
-      el('span', { className: 'card__number' }, 'xxxx xxxx xxxx xxxx'),
-      el('div', { className: 'card__personal' }, [
-        el('span', { className: 'card__name' }, 'John Doe'),
-        el('span', { className: 'card__date' }, '04/24'),
-      ]),
-    ]),
+    creditCard,
+
     el('form', { className: 'form', action: '#', id: 'form' }, [
       el('div', { className: 'form__input-wrap form__input-wrap_holder' }, [
         el(
@@ -19,7 +35,13 @@ const cardData = () => {
           { className: 'form__label form__holder-label' },
           'Card Holder',
         ),
-        el('input', { className: 'input input__holder', type: 'text' }),
+        el('input', {
+          className: 'input input__holder',
+          type: 'text',
+          oninput() {
+            cardName.innerHTML = this.value;
+          },
+        }),
       ]),
       el('div', { className: 'form__input-wrap form__input-wrap_number' }, [
         el(
@@ -27,16 +49,36 @@ const cardData = () => {
           { className: 'form__label form__number-label' },
           'Card Number',
         ),
-        el('input', { className: 'input input__number', id: 'cardNumber' }),
+        el('input', {
+          className: 'input input__number',
+          id: 'cardNumber',
+          oninput() {
+            this.value = creditcardutils.formatCardNumber(this.value);
+            cardNumber.innerHTML = this.value;
+          },
+        }),
       ]),
-      el('div', { className: 'form__input-wrap form__input-wrap_date' }, [
-        el(
-          'label',
-          { className: 'form__label form__date-label' },
-          'Card Expire',
-        ),
-        el('input', { className: 'input input__date', type: 'text' }),
-      ]),
+      el(
+        'div',
+        {
+          className: 'form__input-wrap form__input-wrap_date',
+        },
+        [
+          el(
+            'label',
+            { className: 'form__label form__date-label' },
+            'Card Expire',
+          ),
+          el('input', {
+            className: 'input input__date',
+            type: 'text',
+            oninput() {
+              this.value = creditcardutils.formatCardExpiry(this.value);
+              cardDate.innerHTML = this.value;
+            },
+          }),
+        ],
+      ),
       el('div', { className: 'form__input-wrap form__input-wrap_cvv' }, [
         el('label', { className: 'form__label form__cvv-label' }, 'CVV'),
         el('input', { className: 'input input__cvv', type: 'text' }),
@@ -44,9 +86,7 @@ const cardData = () => {
       el('button', { className: 'form__button' }, 'CHECK OUT'),
     ]),
   );
-
   return el('div', { className: 'wrapper' }, [card]);
 };
 
 setChildren(document.body, cardData());
-console.log(cardData());
